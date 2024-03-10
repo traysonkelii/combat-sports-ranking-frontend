dfcd0f7bebb0e9495fb3cab6ec095e43d363b1f9
traysonkelii <traysonkelii@gmail.com>
2024-03-10 13:24:14 -0700

    fixing the auth relative path

---
+++ src/app/api/tournament/get-all/route.ts
@@ -0,0 +1,6 @@
+// export async function GET() {
+//   const res = await fetch("https://random-api.com/...");
+//   const data = await res.json();
+
+//   return Response.json({ data });
+// }
--- src/app/tournament/page.tsx
+++ src/app/tournament/page.tsx
@@ -1,11 +1,32 @@
 "use client";
 import React from "react";
-import WithAuth from "../components/hoc/withAuth";
+import WithAuth from "@/app/components/hoc/WithAuth";
 
 const Tournament = () => {
+  // const fetchMyData = async () => {
+  //   try {
+  //     const response = await fetch("/api/users/get-by-role", {
+  //       method: "POST",
+  //       headers: {
+  //         Accept: "application/json",
+  //         method: "POST",
+  //       },
+  //       body: JSON.stringify({ role: "COACH" }),
+  //     });
+  //     if (response) {
+  //       console.log(response);
+  //       const data = await response.json();
+  //       console.log(data);
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
   return (
     <WithAuth>
-      <div>Tournament</div>
+      <div>Tournaments</div>
+      <button>test</button>
     </WithAuth>
   );
 };
